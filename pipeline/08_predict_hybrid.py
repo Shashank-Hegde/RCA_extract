c0de37b8e55fd83a056fa9028f1b01ea68b1715d
Shashank Hegde <32035347+Shashank-Hegde@users.noreply.github.com>
2025-04-21 14:17:05 +0200

    Update 08_predict_hybrid.py

--- pipeline/08_predict_hybrid.py
+++ pipeline/08_predict_hybrid.py
@@ -6,7 +6,7 @@ import sys, os, json, torch, argparse
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import feature_utils
-from train_hybrid import SymptomNetHybrid, ENCODER_NAME
+from train_hybrid2 import SymptomNetHybrid, ENCODER_NAME
 from transformers import AutoTokenizer
 from sklearn.preprocessing import LabelEncoder
 
